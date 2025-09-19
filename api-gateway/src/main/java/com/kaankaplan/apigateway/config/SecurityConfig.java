62cb0e3746756a0804166305ede53afeb2779a6e
Darsh616 <darshankatkam@gmail.com>
2025-09-19 22:51:04 +0530

    Containerized the application!

--- api-gateway/src/main/java/com/kaankaplan/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/kaankaplan/apigateway/config/SecurityConfig.java
@@ -2,41 +2,39 @@ package com.kaankaplan.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 
 @Configuration
-@EnableWebFluxSecurity
-@Slf4j
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        log.error("Inside spring security filter chain method");
-        serverHttpSecurity.cors().and().csrf().disable()
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        .anyExchange()
-                        .permitAll());
-        return serverHttpSecurity.build();
+                        .pathMatchers("/eureka/**").permitAll()
+                        .anyExchange().permitAll()
+                )
+                .build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        log.error("Inside corsConfigurationSource");
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");      // allow all origins
+        config.addAllowedMethod("*");      // allow all HTTP methods
+        config.addAllowedHeader("*");      // allow all headers
+        config.setExposedHeaders(Arrays.asList("Authorization")); // optional
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
     }
 }
